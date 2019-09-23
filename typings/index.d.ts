5f96426caab15a7f1778d3772257766b4c7216d6
Kevin Rice <kevinrice@Kevins-MacBook-Pro.local>
2019-09-23 10:16:00 -0500

    initial typings

---
+++ typings/index.d.ts
@@ -0,0 +1,6 @@
+declare namespace Platform {
+    namespace Function {
+        function DeleteData(dataExtName: string, columnNames: string[], valueNames: string[]): void;
+        function InsertData(dateExtName: string, columnNames: string[], valueNames: string[]): void; 
+    }
+}
